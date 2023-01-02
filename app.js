44d2c83f89e4af30b5838e86c5e62eb614baf12c
Konrad Bujas <kondziobu@gmail.com>
2023-01-02 09:23:10 +0100

    state local storage - points

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 console.log('hello!');
+
+
+const playerWinsLSKey = "playerWins";
+const AIWinsLSKey = "AIWins";
+
+let state = {
+    playerWins: localStorage.getItem(playerWinsLSKey) || 0,
+    AIWins: localStorage.getItem(AIWinsLSKey) || 0,
+};
+
+const renderScore = () => {
+    const pointsElement = document.querySelector(".points");
+    pointsElement.innerText = state.playerWins - state.AIWins;
+};
+
+
+const  init = () => {
+    renderScore();
+};
+
+init();
