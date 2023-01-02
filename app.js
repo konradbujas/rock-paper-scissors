a6f0b08d031b6ffd2dd4ba9e90631b684d5cae28
Konrad Bujas <kondziobu@gmail.com>
2023-01-02 10:35:29 +0100

    JS state, AI pick options

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const AIWinsLSKey = "AIWins";
 let state = {
     playerWins: localStorage.getItem(playerWinsLSKey) || 0,
     AIWins: localStorage.getItem(AIWinsLSKey) || 0,
+    playerPick: null,
+    AIPick: null,
 };
 
 const renderScore = () => {
@@ -14,9 +16,41 @@ const renderScore = () => {
     pointsElement.innerText = state.playerWins - state.AIWins;
 };
 
+const bindPickEvents = () => {
+    document.querySelectorAll(".options button").forEach((button) => {
+        button.addEventListener("click", (e) => {
+            pickByPlayer(e.currentTarget.dataset.pick);
+            pickByAI();
+            
+            console.log(state);
+        });
+    });
+};
+
+const pickByPlayer = (pickedOption) => {
+    state = {
+        ...state,
+        playerPick: pickedOption,
+    };
+
+};
+
+const pickByAI = () => {
+    const options = ["rock", "paper", "scissors"];
+    const AIPick = options[Math.floor(Math.random() * options.length)];
+
+    state = {
+        ...state,
+        AIPick,
+    };
+}
+
+
+
 
 const  init = () => {
     renderScore();
+    bindPickEvents();
 };
 
 init();
