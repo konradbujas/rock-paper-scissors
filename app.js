be26beaecee2836c3b8a327e91bf7602247e29db
Konrad Bujas <kondziobu@gmail.com>
2023-01-02 13:54:56 +0100

    JS player pick - createElement

--- app.js
+++ app.js
@@ -18,13 +18,15 @@ const renderScore = () => {
 
 const bindPickEvents = () => {
     document.querySelectorAll(".options button").forEach((button) => {
-        button.addEventListener("click", (e) => {
-            pickByPlayer(e.currentTarget.dataset.pick);
-            pickByAI();
-            
-            console.log(state);
+        button.addEventListener("click", pick);
         });
-    });
+};
+
+const pick = (e) => {
+    pickByPlayer(e.currentTarget.dataset.pick);
+    pickByAI();
+    hideOptions();
+    showFight();
 };
 
 const pickByPlayer = (pickedOption) => {
@@ -43,8 +45,40 @@ const pickByAI = () => {
         ...state,
         AIPick,
     };
-}
+};
 
+const hideOptions = () => {
+    document.querySelector(".options").classList.add("hidden");
+};
+
+const showFight = () => {
+    document.querySelector(".fight").classList.remove("hidden");
+    createElementPickByPlayer();
+    // createElementPickByAI();
+};
+
+const createElementPickByPlayer = () => {
+    const playerPick = state.playerPick;
+    
+    const buttonElement = document.createElement("div");
+    buttonElement.classList.add("button", `button--${playerPick}`);
+
+    const imageContainerElement = document.createElement("div");
+    imageContainerElement.classList.add("button__image-container");
+
+    const imgElement = document.createElement("img");
+    imgElement.src = `./images/icon-${playerPick}.svg`;
+    imgElement.alt = playerPick;
+
+    imageContainerElement.appendChild(imgElement);
+
+    buttonElement.appendChild(imageContainerElement);
+
+    const pickContainerElement = document.querySelector(".pick__container--player");
+    pickContainerElement.innerHTML = "";
+    pickContainerElement.appendChild(buttonElement);
+
+};
 
 
 
